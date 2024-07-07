5faec85076e01fdc8a8ab59f7422baee251c99da
MrChrister08 <102999712+MrChrister08@users.noreply.github.com>
2024-07-07 16:16:17 -0500

    Delete js directory

--- js/scripts.js
+++
@@ -1,9 +0,0 @@
-// scripts.js
-
-function toggleMenu() {
-    const nav = document.querySelector('nav ul');
-    nav.classList.toggle('active');
-
-    const menuToggle = document.querySelector('.menu-toggle');
-    menuToggle.classList.toggle('active');
-}
