44566826255a248f7b939125526f1fd8cc488bec
MrChrister08 <102999712+MrChrister08@users.noreply.github.com>
2024-07-06 20:33:39 -0500

    Delete js directory

--- js/scripts.js
+++
@@ -1,5 +0,0 @@
-function toggleMenu() {
-    var sideMenu = document.getElementById('side-menu');
-    var menuToggle = document.querySelector('.menu-toggle');
-    menuToggle.classList.toggle('open');
-}
